8e03cfcafca97977ab49c59e0f308d4fab99e5ac
Сергей <scorn17@gmail.com>
2016-11-22 00:08:19 +0300

    Create mysettings.php

---
+++ Sublime/mysettings.php
@@ -0,0 +1,14 @@
+{
+	"color_scheme": "Packages/Material Theme/schemes/Material-Theme.tmTheme",
+	"font_face": "Droid Sans Mono",
+	"highlight_line": true,
+	"ignored_packages":
+	[
+		"Vintage"
+	],
+	"line_padding_bottom": 2,
+	"line_padding_top": 2,
+	// "spacegray_fileicons": true,
+	"theme": "Material-Theme.sublime-theme",
+	"wrap_width": 120
+}
